a65146fdae0561b9078a3794098d5bdb44726f0a
hpbcrowe <benjamin.crowe@comcast.net>
2022-06-07 00:05:27 -0600

    ApplicationUser ApplicationUserLogon Models

---
+++ CroweQuest.Models/Account/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿/**********************************
+ *  MODEL FOR A USER'S ACCOUNT
+ *  HOLDS  PROPERTIES FROM THE APPLICATION USER TABLE
+ *  IN THE SQL DATABASE
+ *  FOR USE ON THE APPLICATION
+ *  This was taken from a Udemy Course
+ *  Building a Blog with ASP.NET and Angular
+ *  Data isn't loaded from this model directly
+ *  Token is generated.
+ * *******************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CroweQuest.Models.Account
+{
+    public class ApplicationUser
+    {
+        public int ApplicationUserId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ CroweQuest.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,31 @@
+﻿/**********************************
+ *  MODEL TO LOG IN A USER
+ *  HOLDS  PROPERTIES FROM THE APPLICATION USER TABLE
+ *  IN THE SQL DATABASE
+ *  TO LOG IN A USER
+ *  This was taken from a Udemy Course
+ *  Building a Blog with ASP.NET and Angular
+ * 
+ * *******************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CroweQuest.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage = "Username is Required")]
+        [MinLength(5, ErrorMessage = "Must Be At Least 5 Characters")]
+        [MaxLength(20, ErrorMessage = "Cannot Be More Than 20 Characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(10, ErrorMessage = "Must Be At Least 10 Characters")]
+        [MaxLength(50, ErrorMessage = "Cannot Be More Than 50 Characters")]
+        public string Password { get; set; }
+    }
+}
