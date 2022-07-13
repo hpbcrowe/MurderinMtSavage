4e4e128293fff297397494f1640af13543cc231a
hpbcrowe <benjamin.crowe@comcast.net>
2022-07-12 20:20:53 -0600

    Changes to Angular app before publishing added logged out toastr

--- CroweQuest-UI/src/app/components/navbar/navbar.component.ts
+++ CroweQuest-UI/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -13,15 +14,18 @@ export class NavbarComponent implements OnInit {
 
   constructor(
     public accountService: AccountService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService,
   ) { }
 
   ngOnInit(): void {
   }
 
   logout() {
+    this.toastr.info(` ${this.accountService.currentUserValue.username}` + " Logged Out.");
     this.accountService.logout();
     this.router.navigate(['/']);
+   
   }
 
 }
--- CroweQuest-UI/src/environments/environment.prod.ts
+++ CroweQuest-UI/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  //the address for the api should go here.
+  //webApi: "http://localhost:5000/api"
 };
