07285381d4d46b5f996452fc662f7be3b7013ce7
hpbcrowe <benjamin.crowe@comcast.net>
2022-07-13 22:21:21 -0600

    deployment that worked

--- CroweQuest-UI/src/environments/environment.prod.ts
+++ CroweQuest-UI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //the address for the api should go here.
-  //webApi: "http://localhost:5000/api"
+ 
 };
--- CroweQuest-UI/src/environments/environment.ts
+++ CroweQuest-UI/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   webApi: "http://localhost:5000/api"
+  
 };
 
 /*
