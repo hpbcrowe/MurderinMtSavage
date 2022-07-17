ea7acc534667fb52bca58573f23fcd4ffa31852a
hpbcrowe <benjamin.crowe@comcast.net>
2022-07-16 19:31:28 -0600

    added spinner stretch

--- CroweQuest-UI/src/environments/environment.prod.ts
+++ CroweQuest-UI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //the address for the api should go here.
-  webApi: "https://crowequestweb20220711194507.azurewebsites.net/api"
+  webApi: "something"
 };
