e1c48718cc524d00c0f475e0862dc1a6ad7eff58
hpbcrowe <benjamin.crowe@comcast.net>
2024-11-18 19:03:35 -0700

    Adding comments to token service

--- BlogLab.Services/TokenService.cs
+++ BlogLab.Services/TokenService.cs
@@ -11,7 +11,9 @@ namespace BlogLab.Services
 {
     public class TokenService : ITokenService
     {
+        //This is the key
         private readonly SymmetricSecurityKey _key;
+        //This is the issuer
         private readonly string  _issuer;
 
 
@@ -27,13 +29,18 @@ namespace BlogLab.Services
          * ******************/
         public string CreateToken(ApplicationUserIdentity user)
         {
+            //List of claims
             var claims = new List<Claim>
            {
                new Claim(JwtRegisteredClaimNames.NameId, user.ApplicationUserId.ToString()),
                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
            };
+
+            //need to sign new key pass the key and then sign it cryptographically
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
+            //create the parameters that will go into the function that creates the token
+            //This takes care of the jwt token creation
             var token = new JwtSecurityToken(
                 _issuer,
                 _issuer,
