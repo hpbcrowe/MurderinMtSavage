64fd8c3c2e76c4af6bcb33ddc3ce4534daecccb2
hpbcrowe <benjamin.crowe@comcast.net>
2024-11-30 19:32:27 -0700

    fixing program.cs file so API will run

--- CroweQuest.Web/Program.cs
+++ CroweQuest.Web/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace CroweQuest.Web
 {
@@ -19,14 +18,10 @@ namespace CroweQuest.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
     }
 }
+
