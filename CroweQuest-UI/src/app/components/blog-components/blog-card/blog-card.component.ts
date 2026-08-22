760e490be8b5d44cc4eb012a92127dd12821a217
Ben Crowe <benjamin.crowe@comcast.net>
2026-08-21 22:58:32 -0600

    adding comment box word limit error handling

--- CroweQuest-UI/src/app/components/blog-components/blog-card/blog-card.component.ts
+++ CroweQuest-UI/src/app/components/blog-components/blog-card/blog-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Blog } from 'src/app/models/blog/blog.model';
 import { PhotoService } from 'src/app/services/photo.service';
@@ -24,14 +25,28 @@ export class BlogCardComponent implements OnInit {
 
   ngOnInit(): void {
     if(!!this.blog.photoId){
-      this.photoService.get(this.blog.photoId).subscribe(photo => {
-        //if photo exists then load the imageUrl into the variable created in this service.
-        if(!!photo){
-          this.blogPhotoUrl = photo.imageUrl;
-          this.blogAlt = photo.description; 
-
+      this.photoService.get(this.blog.photoId).subscribe({
+        next: (photo) => {
+          //if photo exists then load the imageUrl into the variable created in this service.
+          if(!!photo){
+            this.blogPhotoUrl = photo.imageUrl;
+            this.blogAlt = photo.description;
+          }
+        },
+        error: (error: HttpErrorResponse) => {
+          const apiError = error?.error;
+          const path = apiError?.Path ?? apiError?.path ?? `/api/Photo/${this.blog.photoId}`;
+          const traceId = apiError?.TraceId ?? apiError?.traceId;
+          console.error('[BlogCard] Failed to load photo.', {
+            blogId: this.blog.blogId,
+            photoId: this.blog.photoId,
+            status: error.status,
+            path,
+            traceId,
+            error,
+          });
         }
-      })
+      });
     }
   }
 
